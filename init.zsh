429250f4ecc690a307cc8a0e60d6594cb2f852bf
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-07-07 13:57:42 -0400

    Initial Commit.

---
+++ init.zsh
@@ -0,0 +1,67 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::version()
+#
+#>
+#####################################################################
+p6df::modules::gcp::version() { echo "0.0.1" }
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::deps()
+#
+#>
+######################################################################
+p6df::modules::gcp::deps() { ModuleDeps=() }
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::external::brew()
+#
+#>
+######################################################################
+p6df::modules::gcp::external::brew() {
+
+  brew cask install google-cloud-sdk
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::langs()
+#
+#>
+######################################################################
+p6df::modules::gcp::langs() {
+
+  pip install gsutil
+  gcloud components install anthoscli beta
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::home::symlink()
+#
+#>
+######################################################################
+p6df::modules::gcp::home::symlink() {
+
+    ln -fs $P6_DFZ_SRC_DIR/$USER/home-private/gcp .gcp
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::gcp::init()
+#
+#>
+######################################################################
+p6df::modules::gcp::init() {
+
+    source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+}
