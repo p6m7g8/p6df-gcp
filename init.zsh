cbd7e78bb70f9289cd1725df86fc81c67c96d608
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-07-10 16:20:46 -0400

    feat(prompt): enables mvp prompt

--- init.zsh
+++ init.zsh
@@ -49,7 +49,8 @@ p6df::modules::gcp::langs() {
 ######################################################################
 p6df::modules::gcp::home::symlink() {
 
-    ln -fs $P6_DFZ_SRC_DIR/$USER/home-private/gcp .gcp
+    ln -fs $P6_DFZ_SRC_DIR/$USER/home-private/gcloud .config/gcloud
+    ln -fs $P6_DFZ_SRC_DIR/$USER/home-private/gsutil .gsutil
 }
 
 ######################################################################
@@ -64,3 +65,28 @@ p6df::modules::gcp::init() {
     source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
     source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 }
+
+p6df::prompt::gcp::line() {
+
+  p6_gcp_prompt_info
+}
+
+## XXX: move to p6gcp
+p6_gcp_prompt_info() {
+
+  # XXX: pick active one
+  local out=$(gcloud projects list | tail -1)
+  local project_id=$(p6_echo "$out" | awk '{print $1}')
+  local name=$(p6_echo "$out" | awk '{print $2}')
+  local project_number=$(p6_echo "$out" | awk '{print $3}')
+
+  if ! p6_string_blank "$name"; then
+    p6_return_str "gcp:    $name ($project_id/$project_number)"
+  fi
+}
+
+# gcloud auth login
+# gcloud config set project PROJECT_ID
+# gcloud projects list
+# gcloud projects describe p6m7g8
+# gcloud configure-docker # Docker credential helper
